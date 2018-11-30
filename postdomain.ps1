1dd530e117dcd6c84c9f66f61006dbfbdcded718
Tyler <tsmith91@asu.edu>
2018-11-29 21:14:34 -0800

    Update for lab3

--- postdomain.ps1
+++ postdomain.ps1
@@ -1,5 +1,14 @@
 # Run this after the machine reboot and check the output for errors
 # tsmith91
+
+# Check the that the DNS server IP address on the NIC is set to our non-loopback IP and set it if needed 
+$nicname = Get-NetAdapter  | select -ExpandProperty "name" 
+$ipaddress = Get-NetIPAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "IPAddress" 
+if ((Get-DnsClientServerAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "ServerAddresses") -ne $ipaddress)  
+{ 
+Set-DnsClientServerAddress -InterfaceAlias $nicname -ServerAddresses ($ipaddress) -Validate 
+}    
+
 dcdiag
 Get-Service adws,kdc,netlogon,dns
 Get-smbshare
